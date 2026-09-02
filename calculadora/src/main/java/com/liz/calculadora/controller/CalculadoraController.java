6017d630b4aa31986272097a95066a82aed919bd
William de Oliveira de Liz <william_liz@estudante.sesisenai.org.br>
2026-09-02 09:21:58 -0300

    adiciona Controller

---
+++ calculadora/src/main/java/com/liz/calculadora/controller/CalculadoraController.java
@@ -0,0 +1,38 @@
+package com.liz.calculadora.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
+public class CalculadoraController {
+
+    private final CalculadoraService calculadoraService;
+
+    public CalculadoraController (CalculadoraController calculadoraController) {
+        this.calculadoraService = calculadoraController.calculadoraService;
+    }
+
+    @PostMapping("/somar")
+    public CalculadoraResponse somar(@RequestBody CalculadoraRequest request){
+        return CalculadoraService.somar(request);
+    }
+
+    @PostMapping("/subtrair")
+    public CalculadoraResponse subtrair(@RequestBody CalculadoraRequest request){
+        return CalculadoraService.subtrair(request);
+    }
+
+    @PostMapping("/multiplicar")
+    public CalculadoraResponse multiplicar(@RequestBody CalculadoraRequest request){
+        return CalculadoraService.multiplicar(request);
+    }
+
+    @PostMapping("/dividir")
+    public CalculadoraResponse dividir(@RequestBody CalculadoraRequest request){
+        return CalculadoraService.dividir(request);
+    }
+
+}
